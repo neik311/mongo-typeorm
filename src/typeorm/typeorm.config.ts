4d40993b0ab6cd30486b19c463e2b3c04abce87d
neik311 <=>
2024-05-03 18:12:27 +0700

    reup env

--- src/typeorm/typeorm.config.ts
+++ src/typeorm/typeorm.config.ts
@@ -3,7 +3,7 @@ require('dotenv').config()
 
 export const dataSource = new DataSource({
   type: 'mongodb',
-  url: 'mongodb+srv://nvkien:g8lrw5mxuy6q68Xe@qluser.qf9zv.mongodb.net/apeLog?retryWrites=true&w=majority',
+  url: `mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@qluser.qf9zv.mongodb.net/${process.env.MONGO_COLLECTION}?retryWrites=true&w=majority`,
   synchronize: true,
   logging: process.env.TYPEORM_LOGGING == 'true',
   entities: [__dirname + '/../entities/*.entity{.ts,.js}'],
